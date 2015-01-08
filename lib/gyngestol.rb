714e51e17185ec7b502bd3cb2ce9aa31b679bd1c
ragmaanir <ragmaanir@gmail.com>
2015-01-09 00:56:52 +0100

    Indentation

--- lib/gyngestol.rb
+++ lib/gyngestol.rb
@@ -12,21 +12,21 @@ module Gyngestol
 
   $LOAD_PATH.unshift(File.join(ROOT, 'lib'))
 
-  autoload :App, 'gyngestol/app'
-  autoload :Action, 'gyngestol/action'
-  autoload :Route, 'gyngestol/route'
-  autoload :Router, 'gyngestol/router'
-  autoload :RouteMatcher, 'gyngestol/router'
-  autoload :RoutingDSL, 'gyngestol/routing_dsl'
-  autoload :RouterBuilder, 'gyngestol/router_builder'
-  autoload :Node, 'gyngestol/node'
-  autoload :InnerNode, 'gyngestol/inner_node'
-  autoload :TerminalNode, 'gyngestol/terminal_node'
+  autoload :App,            'gyngestol/app'
+  autoload :Action,         'gyngestol/action'
+  autoload :Route,          'gyngestol/route'
+  autoload :Router,         'gyngestol/router'
+  autoload :RouteMatcher,   'gyngestol/router'
+  autoload :RoutingDSL,     'gyngestol/routing_dsl'
+  autoload :RouterBuilder,  'gyngestol/router_builder'
+  autoload :Node,           'gyngestol/node'
+  autoload :InnerNode,      'gyngestol/inner_node'
+  autoload :TerminalNode,   'gyngestol/terminal_node'
 
-  autoload :MountPoint, 'gyngestol/mount_point'
-  autoload :Endpoint, 'gyngestol/endpoint'
-  autoload :Escapes, 'gyngestol/escapes'
-  autoload :DSL, 'gyngestol/dsl'
+  autoload :MountPoint,     'gyngestol/mount_point'
+  autoload :Endpoint,       'gyngestol/endpoint'
+  autoload :Escapes,        'gyngestol/escapes'
+  autoload :DSL,            'gyngestol/dsl'
 
   module Doc
     autoload :DocGenerator,     'gyngestol/doc/doc_generator'
